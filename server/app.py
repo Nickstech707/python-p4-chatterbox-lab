b4c2fef1b9cfb3d75a774dd7ecf476610ec5d3fe
Nicholas Muriithi <nickstech707@gmail.com>
2024-07-03 00:25:31 +0300

    Creates an API with Flask for a React frontend application.

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
+from datetime import datetime, timezone
 from models import db, Message
 
 app = Flask(__name__)
@@ -14,13 +15,63 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by('created_at').all()
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            jsonify([message.to_dict() for message in messages]),
+            200,
+        )
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(message.to_dict()),
+            201,
+        )
+
+    return response
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+            
+        message.updated_at = datetime.now(timezone.utc)
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(message.to_dict()),
+            200,
+        )
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response = make_response(
+            jsonify({'deleted': True}),
+            200,
+        )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555)
+
+
